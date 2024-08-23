6b1bf34dedf16a4d40e0e571a2f306f9f032a13f
Mike Warot <chezmike@gmail.com>
2024-08-22 19:12:44 -0500

    add some sample images

--- rmp.py
+++ rmp.py
@@ -1,13 +1,18 @@
 from aesthetic_predictor import predict_aesthetic
 from PIL import Image
 import os
+
 q = os.scandir('.')
 for z in q:
     if z.is_file():
-        if z.name.endswith('JPG'):
-            t = predict_aesthetic(Image.open(z.path))
-            v = t.item()  # strip out the floating point score
-            print(z.name, v)
+        if z.name.lower().endswith(('jpg', 'jpeg')):
+            try:
+                with Image.open(z.path) as img:
+                    t = predict_aesthetic(img)
+                    v = t.item()  # strip out the floating point score
+                    print(z.name, v)
+            except Exception as e:
+                print(f"Error processing file {z.name}: {e}")
     elif z.is_dir():
         print("Found directory ", z.name)
 
